cd7cac0efb0ade2c18b22491084f5c166c8282e4
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-04-09 19:29:06 +0200

    html for home

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../styles/index.scss';
+import Logo from '../components/Logo.js'
+
+const Home = ({content}) => {
+    return (
+        <div className='home'>
+            <Logo />
+            <section>
+                <div className="title">
+                    {content.title} }
+                </div>
+                <div className="body">
+                    {content.content.map((block, i) => (
+                        <p>
+                            {block}
+                        </p>
+                    ))}
+                </div>
+                <a className="button" href="/">
+                    start
+                </a>
+            </section>
+        </div>
+    )
+}
+
+export default Home
