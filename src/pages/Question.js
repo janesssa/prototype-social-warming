e7d38a4fb3c965a032724e50d4a58c3c3a22be9d
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-04-10 22:48:08 +0200

    Added previous button

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -8,6 +8,7 @@ const Question = () => {
     const categories = ['food', 'sleep', 'sport', 'misc'];
     let currentCategory = 0;
     let currentQuestion = 1;
+    let previousButton;
 
     let categoryObject = questions[categories[currentCategory]]
     let questionObject = categoryObject.questions[currentQuestion]
@@ -18,16 +19,29 @@ const Question = () => {
         questionObject = categoryObject.questions[currentQuestion]
         //als currentquestion niet in de het object staat next category
     }
+    const previousQuestion = () => {
+        currentQuestion --
+    }
 
     const questionNumber = () => {
         return currentQuestion < 10 ? '0' + currentQuestion : currentQuestion
     }
+
+        if (currentQuestion != 1) {
+            previousButton = <div className="previous-button" onClick={() => previousQuestion()}>
+            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0.292893 7.29289C-0.0976314 7.68342 -0.0976315 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928931C7.68054 0.538407 7.04738 0.538407 6.65686 0.928931L0.292893 7.29289ZM17 7L1 7L1 9L17 9L17 7Z" fill="black" fill-opacity="0.5"/>
+            </svg>
+        </div>  
+        }
+
     return (
         <div className='questions'>
             <Header />
             <div className="background-number">
                 { questionNumber() }
             </div>
+                {previousButton}
             <div className="questions-body">
                 <div className="category-name">
                     { categoryObject.title }
