ddb911efbe8a3cba6733acbe33b2127994656b3c
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-04-10 16:37:26 +0200

    Create Question page

---
+++ src/pages/Question.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/index.scss';
+import Logo from '../components/Logo.js'
+
+const Question = ({content}) => {
+    return (
+        <div className=''>
+            <Logo />
+        </div>
+    )
+}
+
+export default Question
