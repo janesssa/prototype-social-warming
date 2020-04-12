c2b8ac5fbb86ebd1b1eaa4fb1c8cdb58364064cc
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-04-12 22:35:45 +0200

    Moved previous button to component and added redirect to home

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -4,12 +4,12 @@ import Header from '../components/Header.js'
 import  {questions} from '../config';
 import Answer from '../components/Answer.js'
 import '../styles/questions.scss';
+import PreviousButton from '../components/PreviousButton.js';
 
 const Question = () => {
     const categories = ['food', 'sleep', 'sport', 'misc'];
     const [currentCategory, setCurrentCategory] = useState(0)
     const [currentQuestion, setCurrentQuestion] = useState(1)
-    let previousButton;
 
     let categoryObject = questions[categories[currentCategory]]
     let questionObject = categoryObject.questions[currentQuestion]
@@ -19,7 +19,7 @@ const Question = () => {
         setCurrentQuestion(currentQuestion + 1)
         if (!((currentQuestion+1) in categoryObject.questions)) {
             if (currentQuestion === 10) {
-                console.log('finish test')
+                window.location.href = "/results";
             } else {
                 setCurrentCategory(currentCategory + 1)
             }
@@ -27,7 +27,10 @@ const Question = () => {
     }
     const previousQuestion = () => {
         setCurrentQuestion(currentQuestion - 1)
-        if (!((currentQuestion-1) in categoryObject.questions)) {
+        if (currentQuestion === 1) {
+            window.location.href = "/";
+        }
+        else if (!((currentQuestion-1) in categoryObject.questions)) {
             setCurrentCategory(currentCategory - 1)
         }
     }
@@ -36,21 +39,13 @@ const Question = () => {
         return currentQuestion < 10 ? '0' + currentQuestion : currentQuestion
     }
 
-        if (currentQuestion !== 1) {
-            previousButton = <div className="previous-button" onClick={() => previousQuestion()}>
-            <svg width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M0.292893 7.29289C-0.0976314 7.68342 -0.0976315 8.31658 0.292892 8.70711L6.65685 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928931C7.68054 0.538407 7.04738 0.538407 6.65686 0.928931L0.292893 7.29289ZM17 7L1 7L1 9L17 9L17 7Z" fill="black" fill-opacity="0.5"/>
-            </svg>
-        </div>  
-        }
-
     return (
         <div className='questions'>
             <Header />
             <div className="bg-number">
                 { questionNumber() }
             </div>
-                {previousButton}
+            <PreviousButton previousQuestion={previousQuestion}/>
             <div className="questions-body">
                 <div className="category-name">
                     { categoryObject.title }
