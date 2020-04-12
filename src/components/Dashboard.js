2c42130cec410342a00f6271a6774ce7816ed315
janessa labeur <0935165@hr.nl>
2020-04-12 16:39:25 +0200

    [TASK] Added Webcam element

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,16 @@
 import React from "react";
+import Webcam from "react-webcam";
 
 const Dashboard = () => {
     return (
         <div className='flex-container'>
-            <div>
+            <div className='sm-container'>
                 <p className='bg-number'>€73</p>
                 <h1>Dit ben jij</h1>
-                <p>Maar eigenlijk ook veel meer dan alleen jij. Dit is hoe (toekomstige) verzekeringsmaatschappijen jou zien. Als data. Wat eigenlijk nog interessanter is, is wat ze hier uit afleidenbundleRenderer.renderToStream</p>
+                <p>Maar eigenlijk ook veel meer dan alleen jij. Dit is hoe (toekomstige) verzekeringsmaatschappijen jou zien. Als data. Wat eigenlijk nog interessanter is, is wat ze hier uit afleiden.</p>
                 <button className='button button--red'>Wat betekent dit?</button>
             </div>
-            <video autoplay="true" id="videoElement">
-	
-	</video>
+            <Webcam />
         </div>
     )
 }
