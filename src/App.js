6f07e9acaf65a158fe8c475110e35ba72ed6bf00
janessa labeur <0935165@hr.nl>
2020-04-14 20:04:39 +0200

    [TASK] Added handleNext logic to App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,109 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import {intro} from "./config";
-import Home from './pages/Home';
-import Header from './components/Header'
-import Dashboard from './components/Dashboard';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { intro, questions } from "./config";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Question from "./components/Question";
+import Dashboard from "./components/Dashboard";
 
-import Results from './pages/Results';
+import Results from "./pages/Results";
+
+// const [width, setWidth] = useState(10);
+// const [style, setStyle] = useState();
+// const [question, setquestion] = useState(0);
+// const [text, setText] = useState(questions[question]);
+
+// const handleNext = () => {
+//   if (width < 101 && question < questions.length) {
+//     setquestion((s) => s + 1);
+//     setWidth((w) => w + 9);
+//     setStyle({ width: width + "%" });
+//     setText(questions[question]);
+//   }
+// };
 
 const App = () => {
+  const [index, setIndex] = useState(0);
+  const [text, setText] = useState("");
+  const [style, setStyle] = useState({});
+  const [isHome, toggleIsHome] = useState(true);
+  const [isQuestion, toggleIsQuestion] = useState(false);
+  const [isResults, toggleIsResults] = useState(false);
+  const [isDashboard, toggleIsDashboard] = useState(false);
+
+  const length = questions.length;
+
+    const handleNext = () => {
+        
+        if(index === 0 && isHome){
+            // Move from home screen to first question
+            console.log('home', index, length, text)
+            toggleIsHome(false);
+            toggleIsQuestion(true);
+            setText(`Vraag ${index + 1} van de ${length}`);
+        } else if (index === 0 && isQuestion) {
+            // Move from 1st question to 2nd
+            console.log('1st', index, length, text)
+            setIndex((i) => i + 1);
+            setText(`Vraag ${index + 1} van de ${length}`);
+        } else if (index < length) {
+            // Update index untill end of questions
+            console.log('all questions', index, length, text)
+            setIndex((i) => i + 1);
+            setText(`Vraag ${index + 1} van de ${length}`);
+        } else if (index === length){
+            // Move from last question to dashboard
+            console.log('dashboard', index, length, text)
+            toggleIsDashboard(true);
+            toggleIsQuestion(false);
+            toggleIsResults(false);
+            setText('Resultaten')
+        } else if (index === length + 2){
+            // Move from dashboard to results
+            console.log('results', index, length, text)
+            toggleIsResults(true);
+            toggleIsDashboard(false);
+            toggleIsQuestion(false);
+            setText('Wat betekent dit voor jou?')
+        }
+        console.log('end', index, text)
+    }
+
     return (
         <Router>
-    <Switch>
-      <Route exact path='/'>
-        <Home content={intro} />  
-      </Route>
-      {/* Todo: This can be rebuild --- Render Components depending on the state of the Header */}
-      <Route path='/results'>
-        <Header />
-        {/* Todo: Any other name suggestion is welcome */}
-        <Dashboard />
-      </Route>
-      <Route path='/what-does-it-mean'>
-        <Header />
-        <Results />
-      </Route>
-    </Switch>
-  </Router>
-    )
-}
+        <Switch>
+            <Route exact path="/">
+            {isHome && <Home content={intro} handleNext={handleNext} />}
 
-export default App
+            {!isHome && (
+                <>
+                <Header text={text} style={style} handleNext={handleNext} />
+                {isQuestion && (
+                    <Question
+                    content={questions[index]}
+                    index={index}
+                    handleNext={handleNext}
+                    />
+                )}
+                {isDashboard && <Dashboard handleNext={handleNext} />}
+                {isResults && <Results />}
+                </>
+            )}
+            </Route>
+            <Route path="/results"></Route>
+            {/* Todo: This can be rebuild --- Render Components depending on the state of the Header
+                <Route path="/results">
+                <Header />
+                Todo: Any other name suggestion is welcome
+                <Dashboard />
+                </Route>
+                <Route path="/what-does-it-mean">
+                <Header />
+                <Results />
+                </Route> */}
+        </Switch>
+        </Router>
+    );
+};
+
+export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import "../styles/results.scss";
 
-const Header = () => {
-    const sections = ['Vraag 1 van 15','Vraag 2 van 15','Vraag 2 van 15','Vraag 3 van 15','Vraag 4 van 15','Vraag 1 van 15','Vraag 2 van 15','Vraag 3 van 15','Vraag 5 van 15','Resultaten', 'Wat betekent dit?']
-    const [width, setWidth] = useState(10)
-    const [style, setStyle] = useState()
-    const [section, setSection] = useState(0)
-    const [text, setText] = useState(sections[section])
-
-    const handleNext = () => {
-        if(width < 101 && section < sections.length) {
-            setSection((s) => s + 1)
-            setWidth((w) => w + 9)
-            setStyle({width: width + '%'})
-            setText(sections[section])
-        } 
-    }
-    
-    return (
-        <>
-            <div className="header">
-                <h3>product naam</h3>
-                <small onClick={() => handleNext()}>{text}</small>
-            </div>
-            <div className="progressbar">
-                <div className="inner-progressbar" style={style}></div>
-            </div>
-        </>
-    );
+const Header = ({ text, style, handleNext}) => {
+  return (
+    <>
+      <div className="header">
+        <h3>product naam</h3>
+        <small onClick={() => handleNext()}>{text}</small>
+      </div>
+      <div className="progressbar">
+        <div className="inner-progressbar" style={style}></div>
+      </div>
+    </>
+  );
 };
 
 export default Header;
