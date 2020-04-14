2642d14b2194c7cfda0ac18f07323b381e4b4280
Sweetpurple616 <zoedewaard@hotmail.nl>
2020-04-14 13:58:11 +0200

    Changed anserObject to data

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -3,13 +3,13 @@ import React, {useState}from "react";
 
 const Answer = (data) => {
     const [selected, setSelected] = useState(false)
-    if (answerObject.content.img) {
+    if (data.content.img) {
         return (
             <img
                 onClick={() => setSelected(!selected)}
                 className={`answer-img ${selected ? 'answer-img--active' : ''}`}
-                src={process.env.PUBLIC_URL + answerObject.content.img.src}
-                alt={answerObject.content.img.alt}
+                src={process.env.PUBLIC_URL + data.content.img.src}
+                alt={data.content.img.alt}
             />
         )
 
@@ -18,7 +18,7 @@ const Answer = (data) => {
             <div 
                 onClick={() => setSelected(!selected)}
                 className={`answer ${selected ? 'answer--active' : ''}`}> 
-                {answerObject.content.answer}
+                {data.content.answer}
             </div>
         )
     }
